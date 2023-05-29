2e58efa0eda0cba20921736484d70b63dc4f6df6
VFergDev <vianaferguson@yahoo.com>
2023-05-29 11:33:15 -0700

    real form add

--- .history/assets/js/main_20230529110912.js
+++ .history/assets/js/main_20230529110912.js
@@ -258,21 +258,20 @@
      * Initiate Pure Counter 
      */
     new PureCounter();
-    
+    const submitContactForm = ()=>{
+      Email.send({
+        Host : "smtp.gmail.com",
+        Port : "465"
+        Username : "colormehighco@gmail.com",
+        Password : "ximhzscrxucoyvrh",
+        To : 'djveev86@gmail.com',
+        From : document.getElementById("email").value,
+        Subject : document.getElementById("subject").value,
+        Body : "And this is the body"
+    }).then(
+      message => alert(message)
+    );
+    }
   
   })()
 
-  // const submitContactForm = ()=>{
-  //   Email.send({
-  //     Host : "smtp.gmail.com",
-  //     Port : "465",
-  //     Username : "colormehighco@gmail.com",
-  //     Password : "ximhzscrxucoyvrh",
-  //     To : 'djveev86@gmail.com',
-  //     From : document.getElementById("email").value,
-  //     Subject : document.getElementById("subject").value,
-  //     Body : "And this is the body"
-  // }).then(
-  //   message => alert(message)
-  // );
-  // }
